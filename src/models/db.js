265494b43a5e83261f22691ea46dfe9019397013
Benjamin H Boruff <ben@boruff.me>
2018-03-04 18:38:13 -0500

    initial commit

---
+++ src/models/db.js
@@ -0,0 +1,7 @@
+var mongoose = require('mongoose');
+
+const { database, secret } = require('../config');
+mongoose.Promise = global.Promise;
+mongoose.connect(database);
+
+module.exports = mongoose;
